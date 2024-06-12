1096863b52013fc0b55184eca6a3d2c54b26187e
Leah Nyambura <leahnyambura682@gmail.com>
2024-06-12 23:41:16 +0300

    Dashboard UI in Full project

---
+++ Full-project/routes/web.php
@@ -0,0 +1,14 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ApplicationController;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+// Route::get('/admin/dashboard', function () {
+//     return view('dashboard');
+// });
+
+Route::get('{view}',ApplicationController::class)->where('view', '(.*)');
