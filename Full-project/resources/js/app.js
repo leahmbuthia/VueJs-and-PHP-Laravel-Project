c6fc1db983c53a127ac3087bf9d6f1790c0ffc89
Leah Nyambura <leahnyambura682@gmail.com>
2024-06-13 16:19:09 +0300

    Created user modal and post get render all

--- Full-project/resources/js/app.js
+++ Full-project/resources/js/app.js
@@ -1,5 +1,4 @@
 import './bootstrap';
-import 'admin-lte/plugins/jquery/jquery.min.js'
 import 'admin-lte/plugins/bootstrap/js/bootstrap.bundle.min.js'
 import 'admin-lte/dist/js/adminlte.min.js?v=3.2.0'
 import {createApp} from 'vue/dist/vue.esm-bundler.js';
