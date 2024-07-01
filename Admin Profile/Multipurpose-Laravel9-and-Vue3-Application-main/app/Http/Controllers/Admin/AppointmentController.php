d991c4b6377b0ffd9ecea81d2e8910dfd3396d46
Leah Nyambura <leahnyambura682@gmail.com>
2024-07-01 11:25:23 +0300

    Introduction to the RestApi in laravel

---
+++ Admin Profile/Multipurpose-Laravel9-and-Vue3-Application-main/app/Http/Controllers/Admin/AppointmentController.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Enums\AppointmentStatus;
+use App\Http\Controllers\Controller;
+use App\Models\Appointment;
+
+class AppointmentController extends Controller
+{
+    public function index()
+    {
+        return Appointment::query()
+            ->with('client:id,first_name,last_name')
+            ->when(request('status'), function ($query) {
+                return $query->where('status', AppointmentStatus::from(request('status')));
+            })
+            ->latest()
+            ->paginate()
+            ->through(fn ($appoinment) => [
+                'id' => $appoinment->id,
+                'start_time' => $appoinment->start_time->format('Y-m-d h:i A'),
+                'end_time' => $appoinment->end_time->format('Y-m-d h:i A'),
+                'status' => [
+                    'name' => $appoinment->status->name,
+                    'color' => $appoinment->status->color(),
+                ],
+                'client' => $appoinment->client,
+            ]);
+    }
+
+    public function store()
+    {
+        $validated = request()->validate([
+            'client_id' => 'required',
+            'title' => 'required',
+            'description' => 'required',
+            'start_time' => 'required',
+            'end_time' => 'required',
+        ], [
+            'client_id.required' => 'The client name field is required.',
+        ]);
+
+        Appointment::create([
+            'title' => $validated['title'],
+            'client_id' => $validated['client_id'],
+            'start_time' => $validated['start_time'],
+            'end_time' => $validated['end_time'],
+            'description' => $validated['description'],
+            'status' => AppointmentStatus::SCHEDULED,
+        ]);
+
+        return response()->json(['message' => 'success']);
+    }
+
+    public function edit(Appointment $appointment)
+    {
+        return $appointment;
+    }
+
+    public function update(Appointment $appointment)
+    {
+        $validated = request()->validate([
+            'client_id' => 'required',
+            'title' => 'required',
+            'description' => 'required',
+            'start_time' => 'required',
+            'end_time' => 'required',
+        ], [
+            'client_id.required' => 'The client name field is required.',
+        ]);
+
+        $appointment->update($validated);
+
+        return response()->json(['success' => true]);
+    }
+
+    public function destroy(Appointment $appointment)
+    {
+        $appointment->delete();
+
+        return response()->json(['success' => true], 200);
+    }
+}
--- VueJs Crud Using Laravel Api/src/views/HomeView.vue
+++
@@ -1,9 +0,0 @@
-
-
-<template>
-  <main>
-<h1> Welcome to Vuejs and Larvel </h1>
-  
-  </main>
-</template>
-
